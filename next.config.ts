47ad50e9a066b43d0bbaa65eed38366d97b10b81
Minh Hai Hoang <122735724+Thomas-Hoang-04@users.noreply.github.com>
2026-03-31 12:38:18 +0700

    feat: update application config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['192.168.0.7', '192.168.204.189'],
   reactCompiler: true,
 };
 
